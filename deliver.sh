8365e4432982af082540c3cb065f71ffff2e7bdb
LukasSlizik <lukas.slizik@gmail.com>
2023-11-09 16:25:05 +0100

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -25,4 +25,4 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 #java -jar target/${NAME}-${VERSION}.jar
 
-docker build --tag java-docker .
+sudo docker build --tag java-docker .
